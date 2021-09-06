fd62f5b5e676a580f4cc10cb578ae705b3f7db6e
Sai Krishna <kvskrishna712@gmail.com>
2021-09-06 21:49:16 +0530

    added project

---
+++ src/main/java/com/sprinter/sprintercompany/SprinterCompanyApplication.java
@@ -0,0 +1,14 @@
+package com.sprinter.sprintercompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinterCompanyApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SprinterCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprinter/sprintercompany/SprinterCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprinter.sprintercompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinterCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
